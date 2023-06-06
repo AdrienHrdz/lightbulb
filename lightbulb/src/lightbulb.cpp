d3eee0d0dae1a768dc27c9db4b8e7fa89db08d03
AdrienHrdz <adrienhernandez16@gmail.com>
2023-06-06 21:11:45 +0200

    bon ca marche pas encore

--- lightbulb/src/lightbulb.cpp
+++ lightbulb/src/lightbulb.cpp
@@ -3,6 +3,10 @@
 
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
 
 #include <iostream>
 
@@ -27,6 +31,13 @@ int main()
 
     glewInit();
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 410");
+
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -38,8 +49,55 @@ int main()
 
         /* Poll for and process events */
         glfwPollEvents();
+
+
+        ImGuiIO& io = ImGui::GetIO();
+
+        int n = 20;
+        printf("NewFrame() %d\n", n);
+        io.DisplaySize = ImVec2(1920, 1080);
+        io.DeltaTime = 1.0f / 60.0f;
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui::NewFrame();
+
+        static float f = 0.0f;
+        ImGui::Text("Hello, world!");
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::ShowDemoWindow(nullptr);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+
     }
+    ImGui::DestroyContext();
 
     glfwTerminate();
+
+    //// Build atlas
+    //unsigned char* tex_pixels = nullptr;
+    //int tex_w, tex_h;
+    //io.Fonts->GetTexDataAsRGBA32(&tex_pixels, &tex_w, &tex_h);
+
+    //while (true)
+    //{
+    //    int n = 20;
+    //    printf("NewFrame() %d\n", n);
+    //    io.DisplaySize = ImVec2(1920, 1080);
+    //    io.DeltaTime = 1.0f / 60.0f;
+    //    ImGui::NewFrame();
+
+    //    static float f = 0.0f;
+    //    ImGui::Text("Hello, world!");
+    //    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    //    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+    //    ImGui::ShowDemoWindow(nullptr);
+
+    //    ImGui::Render();
+    //}
+
+    //printf("DestroyContext()\n");
     return 0;
 }
