d94e9b81125b5d8053ed48c339592dc96bac3255
Malkrich <mathurin.lemarie@gmail.com>
2023-06-06 20:06:54 +0200

    add GLEW

--- lightbulb/src/lightbulb.cpp
+++ lightbulb/src/lightbulb.cpp
@@ -1,9 +1,10 @@
 // lightbulb.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
+#include <GL\glew.h>
 #include <GLFW\glfw3.h>
+
+#include <iostream>
 
 int main()
 {
@@ -12,4 +13,33 @@ int main()
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
