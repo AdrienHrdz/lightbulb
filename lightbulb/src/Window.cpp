de986f79f007a2fbd945cb1b470aaa916cf6a687
Malkrich <mathurin.lemarie@gmail.com>
2023-06-11 17:15:09 +0200

    adding ImGui to Window class

--- lightbulb/src/Window.cpp
+++ lightbulb/src/Window.cpp
@@ -1,14 +1,25 @@
 #include "Window.h"
 
-#include <iostream>
-
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#include <iostream>
 
 Window::Window(uint32_t width, uint32_t height, const std::string& title)
 	: m_width(width)
 	, m_height(height)
 	, m_title(title)
-{}
+{
+	initialize();
+}
+
+Window::~Window()
+{
+	shutdown();
+}
 
 void Window::initialize()
 {
@@ -23,10 +34,35 @@ void Window::initialize()
 	}
 
 	glfwMakeContextCurrent(m_window);
+
+	/*****************************************
+					IMGUI
+	*****************************************/
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;        // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // docking
+
+	ImGui::StyleColorsDark();
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+	    style.WindowRounding = 0.0f;
+	    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+	ImGui_ImplOpenGL3_Init("#version 430");
 }
 
 void Window::shutdown()
 {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwDestroyWindow(m_window);
 	glfwTerminate();
 }
@@ -34,7 +70,21 @@ void Window::shutdown()
 void Window::onUpdate()
 {
 	glfwPollEvents();
-	glfwSwapBuffers(m_window);
 
-	std::cout << "on window update" << std::endl;
+	ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+	ImGui::ShowDemoWindow();
+
+	int display_w, display_h;
+    glfwGetFramebufferSize(m_window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	glfwSwapBuffers(m_window);
 }
